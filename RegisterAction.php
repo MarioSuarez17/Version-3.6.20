faba622501885d1079c9aa737af9f1408a76751d
MarioSuarez17 <msuarez1706@gmail.com>
2020-09-12 17:15:31 -0600

    Conf RegisterAction

--- RegisterAction.php
+++ RegisterAction.php
@@ -1,8 +1,7 @@
-
 <?php error_reporting (E_ALL ^ E_NOTICE); ?>
 <?php
 
-require 'Connection.php';
+//  Datos
 $ActionType = $_GET['ActionType'];
 $Location = $_GET["Loc"];
 $Username = $_POST['Username'];
@@ -10,31 +9,31 @@ $Password = $_POST['Password'];
 $Firstname = $_POST['Firstname'];
 $Middlename = $_POST['Middlename'];
 $Lastname = $_POST['Lastname'];
-$Address = $_POST['Address'];
-$Gym = $_POST['Gym'];
+$Telefono = $_POST['Telefono'];
+//$Gym = $_POST['Gym'];
 $EmailAddress = $_POST['EmailAddress'];
 
-if (empty($Username) || empty($Password) || empty($Firstname) || empty($Middlename) || empty($Lastname) || empty($Address) || empty($EmailAddress)|| empty($Gym)) {
+if (empty($Username) || empty($Password) || empty($Firstname) || empty($Middlename) || empty($Lastname) || empty($EmailAddress)|| empty($Telefono)) {
 	echo '<script>window.alert("Cannot leave the page blank"); window.open("register.php","_self",null,true);</script>';
 } else {
 	if ($ActionType == "Register") {
-
+	   require 'Connection.php';
 		//$pass_cifra = password_hash($Password,PASSWORD_BCRYPT);
-		$sql = "INSERT INTO `tbl_customers`(`Username`,`Password`,`Role`,`Firstname`, `Middlename`, `Lastname`, `Address`, `EmailAddress`, `GinmasioID`)" .
-			" VALUES ('$Username','$Password','User','$Firstname','$Middlename','$Lastname','$Address','$EmailAddress',$Gym)";
+		$sql = "INSERT INTO tbl_usuario(usuario,contrasenna,rol,nombre, primerApellido, segundoApellido,telefono,correo) VALUES ('$Username','$Password','User','$Firstname','$Middlename','$Lastname',$Telefono,'$EmailAddress')";
 
-		$res = mysqli_query($Conn, $sql);
+		$res = sqlsrv_query($Conn, $sql);
 		
-		if (!$res) {
-		
-			echo "Failed " . mysqli_connect_error();
+		if ($res) {
+			echo '<script>window.alert("Registro Completado Por favor Ingresar"); window.open("Login.php?Role=User","_self",null,true);</script>';
+			
 		
 		} else {
-			echo '<script>window.alert("Registro Completado Por favor Ingresar"); window.open("Login.php?Role=User","_self",null,true);</script>';
+			echo "Estamos en la piedra";
+			die(print_r( sqlsrv_errors(), true));  
 		}
 	} else {
 		$ID = $_GET['ID'];
-		$sqlI = "UPDATE `tbl_customers` SET `Username`='$Username',`Password`='$Password',`Firstname`='$Firstname'," .
+      	$sqlI = "UPDATE `tbl_customers` SET `Username`='$Username',`Password`='$Password',`Firstname`='$Firstname'," .
 			"`Middlename`='$Middlename',`Lastname`='$Lastname',`Address`='$Address',`EmailAddress`='$EmailAddress' WHERE CustomerID = $ID";
 		$res = mysqli_query($Conn, $sqlI);
 		if (!$res) {
