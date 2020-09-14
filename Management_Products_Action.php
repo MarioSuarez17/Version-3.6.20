a5ee383e68078e4eb71458d174a26d8abbf4b9cf
MarioSuarez17 <msuarez1706@gmail.com>
2020-09-13 18:56:38 -0600

    Trabajando con SQLServer

--- Management_Products_Action.php
+++ Management_Products_Action.php
@@ -11,31 +11,39 @@
 	if($ProductAction == "Add")
 	{
 		$_PlatilloName = $_POST["PlatilloName"];
-		$_PlatilloPrice = $_POST["PlatilloPrice"];
 		$_ProductDescript = $_POST["PlatilloDescript"];
+		$_PlatilloPrice = $_POST["PlatilloPrice"];
+		$_ProductCatidad = $_POST["PlatilloQuantity"];
+		
 		$_Estado = $_POST["Estado"];
-
+		
 		$image = addslashes($_FILES['ProductImage']['tmp_name']);
 		$name = addslashes($_FILES['ProductImage']['name']);
 		$image = file_get_contents($image);
 		$image = base64_encode($image);
+		$_CategoriaID = $_POST["CategoriaID"];
 		
-		$sql = "INSERT INTO `tbl_platillos`(`Platilloname`,`PlatilloDescript`,`PlatilloPrice`, `PlatilloImageName`, `PlatilloImage`, `Estado` )" . 
-		"VALUES ('$_PlatilloName','$_ProductDescript','$_PlatilloPrice','$name','$image','$_Estado')";
-		$res = mysqli_query($Conn,$sql);
+		$sql = "INSERT INTO tbl_producto(nombre,descripcion,precio,cantidad,imageNombre,imagen,estado,categoriaID)" . 
+		"VALUES ('$_PlatilloName','$_ProductDescript','$_PlatilloPrice',$_ProductCatidad,'$name','$image','$_Estado',$_CategoriaID)";   //,
+		$res = sqlsrv_query($Conn,$sql);
 		if($res)
 		{
-			echo '<script>window.open("Management_ProductsList.php","_self",null,true);</script>';
+			echo '<script>window.alert("Product has been successfully created!"),window.open("Management_ProductsList.php","_self",null,true);</script>';
+			     
 		}
 		else
 		{
-			echo '<script>alert("FAILED!")</script>';
+			echo '<script>alert("FAILED!"); window.open("Management_Products.php","_self",null,true)</script>';
+
 		}  //Evalua la accion 
 	}else if($ProductAction == "Edit")
 	{
 		$_PlatilloName = $_POST["PlatilloName"];
+		$_ProductDescript = $_POST["PlatilloDescript"];
 		$_PlatilloPrice = $_POST["PlatilloPrice"];
-		$_PlatilloDescript = $_POST["PlatilloDescript"];
+		$_ProductCatidad = $_POST["PlatilloQuantity"];
+		//$_Estado = $_POST["Estado"];
+		$_CategoriaID = $_POST["CategoriaID"];
 		
 
 		$image = addslashes($_FILES['ProductImage']['tmp_name']);
@@ -43,30 +51,30 @@
 		$image = file_get_contents($image);
 		$image = base64_encode($image);
 		//Toma el ID
-		$_PlatilloID = $_GET["PlatilloID"];
+		$_PlatilloID = $_GET["productoID"];
 	
 		if(empty($_FILES['ProductImage']['tmp_name'])){
-			$sql = "UPDATE `tbl_platillos` SET `Platilloname`='$_PlatilloName',`PlatilloDescript`='$_PlatilloDescript'," .
-				   "`PlatilloPrice`='$_PlatilloPrice' WHERE `PlatilloID` =  $_PlatilloID";
-			$res = mysqli_query($Conn,$sql);
+			$sql = "UPDATE tbl_producto SET nombre='$_PlatilloName',descripcion='$_ProductDescript'," .
+				   "precio=$_PlatilloPrice  WHERE productoID =  $_PlatilloID";
+			$res = sqlsrv_query($Conn,$sql);
 			if($res)
 			{
 				echo '<script>window.alert("Product has been successfully updated!"); window.open("Management_ProductsList.php","_self",null,true)</script>';
 			}
 		}
-		$sql = "UPDATE `tbl_platillos` SET `Platilloname`='$_PlatilloName',`PlatilloDescript`='$_PlatilloDescript'," .
-			   "`PlatilloPrice`='$_PlatilloPrice'," .
-			   "`PlatilloImageName`='$name',`PlatilloImage`='$image' WHERE `PlatilloID` = $_PlatilloID";
-		$res = mysqli_query($Conn,$sql);
+		$sql = "UPDATE tbl_producto SET nombre='$_PlatilloName',descripcion='$_ProductDescript'," .
+			   "precio=$_PlatilloPrice,cantidad=$_ProductCatidad,categoriaID=$_CategoriaID," .
+			   "imageNombre='$name',imagen='$image' WHERE productoID = $_PlatilloID";
+		$res = sqlsrv_query($Conn,$sql);
 		if($res)
 		{
 			echo '<script>window.alert("Product has been successfully updated!"); window.open("Management_ProductsList.php","_self",null,true)</script>';
 		}
 	}else if($ProductAction == "Delete")
 	{//Ver para establer para cambiar de estado REVISAR
-		$_PlatilloID = $_GET["ProdID"];
-		$sql = "UPDATE tbl_platillos SET Estado = 'Desactivo' where PlatilloID = $_PlatilloID";
-		$res = mysqli_query($Conn,$sql);
+		$_PlatilloID = $_GET["productoID"];
+		$sql = "UPDATE tbl_producto SET Estado = 'Desactivo' where productoID = $_PlatilloID";
+		$res = sqlsrv_query($Conn,$sql);
 		if($res)
 		{
 			echo '<script>window.alert("Product has been successfully Deleted!"); window.open("Management_ProductsList.php","_self",null,true)</script>';
