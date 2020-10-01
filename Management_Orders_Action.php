6b8434f8d99466b7c5f3409381ab77aa6f1f640b
MarioSuarez17 <msuarez1706@gmail.com>
2020-09-30 22:12:41 -0600

    Prepare statement listo

--- Management_Orders_Action.php
+++ Management_Orders_Action.php
@@ -1,30 +1,28 @@
 <?php
-
-//reload() Probar 
 	session_start();
+	$OrderAction = $_GET["OrderAction"];
 	require 'Connection.php';
-    //$ID = $_GET["id"];
-    //$sql = "UPDATE tbl_orders SET Estado = 'Cancelado' where OrderID = $ID";
-	//$sql = "sp_query_orden_Cancelar";
-    //$res = sqlsrv_query($Conn,$sql);
-	
-	$_OrderID = $_GET["ordenID"];
-	// Se crean los parámetros
-	$myparams['ordenID'] = $_OrderID;
-	
-	//Se crea un array con de parámetros
+	//ACCIONES 
+	if($OrderAction == "Cancelar"){
+	$_ordenID = $_GET["ordenID"];
+	$myparams['ordenID'] = $_ordenID;
 	$procedure_params = array(
-		array(&$myparams['ordenID'], SQLSRV_PARAM_IN)		
-	);
-
-	$sql = "sp_query_orden_Cancelar @ordenID=?,@estado='Cancelada'";
+		array(&$myparams['ordenID'], SQLSRV_PARAM_IN));
+	$sql = "sp_update_orders_cancelado @ID =?";
 	$stmt = sqlsrv_prepare($Conn, $sql,$procedure_params);
-
-	if(sqlsrv_execute($stmt))
-
-	//if($res)
-	{
-		echo '<script>window.alert("Orden Cancelada!");<script>window.open("Management_Orders.php","_self",null,true);</script>';
+		if(sqlsrv_execute($stmt))
+		{
+			echo '<script>window.alert("The order has been updated successfully!"); window.open("Management_Orders.php","_self",null,true)</script>';
+		}
+	}elseif($OrderAction == "Completo"){
+		$_ordenID = $_GET["ordenID"];
+		$myparams['ordenID'] = $_ordenID;
+		$procedure_params = array(array(&$myparams['ordenID'], SQLSRV_PARAM_IN));
+		$sql = "sp_update_orders_completo @ID =?";
+		$stmt = sqlsrv_prepare($Conn, $sql,$procedure_params);
+			if(sqlsrv_execute($stmt))
+			{
+				echo '<script>window.alert("The order has been updated successfully!"); window.open("Management_Orders.php","_self",null,true)</script>';
+			}
 	}
-	
 ?>
